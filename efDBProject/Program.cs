6c6fbda92250620b72392d80411cfa304e7b0511
kushalkadel <kushalkdl63@gmail.com>
2026-02-11 18:45:42 +0545

    set up db context file for databse operations

--- efDBProject/Program.cs
+++ efDBProject/Program.cs
@@ -1,4 +1,8 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using efDBProject.data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("appDb"));
 
 // Add services to the container.
 
